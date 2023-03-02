620f7d3fb7d645292270bbb2cb489f1de6637288
Genesis Scott <chandlerscott123@gmail.com>
2023-03-02 01:26:57 -0600

    add function, takes user input and returns the sum

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+function add(number1, number2) {
+  return number1+number2;
+}
+
+
+const a = parseInt(window.prompt("Enter a number: "));
+const b = parseInt(window.prompt("Enter another number: "));
+
+window.alert(add(a,b));
