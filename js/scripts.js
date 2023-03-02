8040148d95f00bff83acc88597880c258e768b43
Genesis Scott <chandlerscott123@gmail.com>
2023-03-02 01:38:34 -0600

    expand frontend to display full input and arithmetic operators

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,20 @@ function add(number1, number2) {
   return number1+number2;
 }
 
+function multiply(c, d){
+  return c*d;
+}
 
-const a = parseInt(window.prompt("Enter a number: "));
-const b = parseInt(window.prompt("Enter another number: "));
 
-window.alert(add(a,b));
+
+const a = parseInt(window.prompt("ADD. Enter a number: "));
+const b = parseInt(window.prompt("ADD. Enter another number: "));
+
+const resultAdd= (add(a,b)).toString();
+window.alert(a.toString() + " + " + b.toString() + " = " + resultAdd); 
+
+// window.alert(add(a,b));
+
+// const multA = parseInt(window.prompt("Multiply. Enter a number: "));
+// const multB = parseInt(window.prompt("Multiply. Enter another number: "));
+// window.alert(multiply(multA,multB));
